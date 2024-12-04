87acd2d8393587abb1499fbad6edc5d0b704d818
Ramon Vitor <143614775+Ramon-Vitor@users.noreply.github.com>
2024-12-03 23:00:47 -0300

    Update

--- assets/js/fetchApis.js
+++ assets/js/fetchApis.js
@@ -1,4 +1,4 @@
-API_URL = "https://insta-grama-780703529073.southamerica-east1.run.app/posts"
+const API_URL = "https://insta-grama-780703529073.southamerica-east1.run.app/posts"
 
 let { API_URL } = process.env;
 
