20a000f6244cf63b0a5315cd51d5da038c9ffc80
Ramon Vitor <143614775+Ramon-Vitor@users.noreply.github.com>
2024-12-05 14:38:37 -0300

    Update fetchApis.js

--- assets/js/fetchApis.js
+++ assets/js/fetchApis.js
@@ -21,3 +21,16 @@ fetch('https://insta-grama-api-780703529073.us-east1.run.app'){
     console.error('Erro na requisição:', error);
   })
 };
+async function fetchImages() {
+    try {
+        const response = await fetch('https://insta-grama-api-780703529073.us-east1.run.app'); // URL da API
+        if (!response.ok) {
+            throw new Error('Erro na requisição');
+        }
+        const data = await response.json();
+        return data; // Certifique-se de que a função retorna os dados
+    } catch (error) {
+        console.error('Erro ao buscar as imagens:', error);
+        return []; // Retorne um array vazio ou um valor que o código possa manipular
+    }
+}
