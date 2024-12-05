7ae8dbd694e0e22d781e8a7daf09a2af4a51a872
Ramon Vitor <143614775+Ramon-Vitor@users.noreply.github.com>
2024-12-04 21:17:16 -0300

    Update fetchApis.js

--- assets/js/fetchApis.js
+++ assets/js/fetchApis.js
@@ -1,17 +1,13 @@
-
-import'dotenv/config';
-
-let { API_URL } = process.env;
+let API_URL = process.env.API_URL;
 
 // Função para buscar os dados do endpoint
 export default async function fetchImages() {
   try {
-    const response = await fetch(process.env.API_URL); // Usando a URL importada
+    const response = await fetch(API_URL); // Usando a URL importada
     const data = await response.json();
     return data;
   } catch (error) {
     console.error("Erro ao buscar dados:", error);
-    console.log("Erro ao buscar dados:", error);
   }
 }
 
