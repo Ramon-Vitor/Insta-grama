82ca92c98d91cf0f3cff4b713349780be649c891
Ramon Vitor <143614775+Ramon-Vitor@users.noreply.github.com>
2024-12-05 14:22:56 -0300

    Update fetchApis.js

--- assets/js/fetchApis.js
+++ assets/js/fetchApis.js
@@ -21,15 +21,3 @@ fetch('https://insta-grama-api-780703529073.us-east1.run.app'){
     console.error('Erro na requisição:', error);
   })
 };
-
-.then(response => {
-  console.log(response);
-  if (!response.ok) {
-    throw new Error('Erro na requisição: ' + response.status);
-  }
-  return response.json();
-})
-.then(data => console.log(data))
-.catch(error => {
-  console.error('Erro ao fazer requisição:', error);
-});
